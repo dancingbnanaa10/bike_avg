95a70b2bbd67b5e6c53fe6f76f12d3695d1ab285
Wojciech <waitingfordropbox@gmail.com>
2024-08-15 16:06:30 +0200

    1st

---
+++ js/calc.js
@@ -0,0 +1,8 @@
+const viewportWidth = window.innerWidth;
+const viewportHeight = window.innerHeight;
+const main_C = document.querySelector('#main_Container');
+main_C.style.width = viewportWidth*.4 + "px";
+main_C.style.height = viewportHeight*.7+"px";
+main_C.style.marginLeft = viewportWidth*.3+"px";
+main_C.style.marginTop = viewportHeight*.15+"px";
+
